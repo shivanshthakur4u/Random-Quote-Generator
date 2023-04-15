f2903749236622e04d9d34e8730b3e1db2b76cb0
Saurabh Singh <boyslike2play@gmail.com>
2023-04-15 18:26:37 +0530

    basic file structure created

---
+++ Components/BookmarkList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BookmarkList = () => {
+  return (
+    <div>BookmarkList</div>
+  )
+}
+
+export default BookmarkList
---
+++ Components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+
+  return (
+    <div>
+
+<p className="card-content">{"quote"}</p>
+        <p className="card-author">- {"Author"}</p>
+    </div>
+  )
+}
+
+export default Card
